44c2dc99efc7d9928c6f971912021c26a66b38e3
anggis <anggisdar@gmail.com>
2024-12-26 12:19:43 +0700

    last update

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Simple api running in port 8080")
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("'/status' was called")
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "OK"}`))
+	})
+
+	http.HandleFunc("/", handleRoot)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	log.Println("'/' was called")
+	w.WriteHeader(http.StatusNotImplemented)
 }
